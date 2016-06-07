dd3f934294e5ebc1fc4064d423cac6289d1c6ef6
Amit Aryeh Levy <amit@amitlevy.com>
2016-06-06 19:01:31 -0700

    Bare minimum code for hotel chip
    Makefile, minimal linker script and a reset handler that doesn't even
    copy the data segment or zero the bss segment, just calls main

---
+++ src/chips/hotel/lib.rs
@@ -0,0 +1,27 @@
+#![crate_name = "hotel"]
+#![crate_type = "rlib"]
+#![no_std]
+#![feature(core_intrinsics)]
+
+extern crate support;
+
+extern {
+    // _estack is not really a function, but it makes the types work
+    // You should never actually invoke it!!
+    fn _estack();
+
+    // Defined in src/main/main.rs
+    fn main();
+}
+
+
+#[link_section=".vectors"]
+pub static ISR_VECTOR: [Option<unsafe extern fn()>; 2] = [
+    /* Stack top */     Option::Some(_estack),
+    /* Reset */         Option::Some(reset_handler),
+];
+
+unsafe extern "C" fn reset_handler() {
+    main()
+}
+
