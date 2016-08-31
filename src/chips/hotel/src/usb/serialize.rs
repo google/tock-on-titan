c97969d1e627c30d921a0448ad4fb232d9fc3e2a
Amit Aryeh Levy <amit@amitlevy.com>
2016-08-31 18:31:28 -0400

    Add Serialize trait for USB module to make serializing simpler

---
+++ src/chips/hotel/src/usb/serialize.rs
@@ -0,0 +1,29 @@
+use core::mem::{transmute, size_of};
+use core::intrinsics::copy_nonoverlapping;
+
+pub unsafe trait Serialize: Sized {
+    fn serialize(&self, buffer: &mut [u8]) -> usize {
+        let length = if buffer.len() < size_of::<Self>() {
+            buffer.len()
+        } else {
+            size_of::<Self>()
+        };
+
+        unsafe {
+            copy_nonoverlapping(transmute(self), buffer.as_mut_ptr(), length);
+        }
+        length
+    }
+}
+
+unsafe impl Serialize for u8 {}
+unsafe impl Serialize for u16 {}
+unsafe impl Serialize for u32 {}
+unsafe impl Serialize for u64 {}
+unsafe impl Serialize for usize {}
+unsafe impl Serialize for i8 {}
+unsafe impl Serialize for i16 {}
+unsafe impl Serialize for i32 {}
+unsafe impl Serialize for i64 {}
+unsafe impl Serialize for isize {}
+
