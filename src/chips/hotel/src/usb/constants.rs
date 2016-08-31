c85d8c65d98fe254c83d8c4d9a55f7eb64916893
Amit Aryeh Levy <amit@amitlevy.com>
2016-08-31 11:20:25 -0400

    Fix a bad bug in USB constants
    MAX_PACKET_SIZE should be 64 not 1024. This bug resulted in a
    TX_FIFO_SIZE of 512, which is larger than the hardware maximum as
    specified in coreConsultant, which results in the hardware just using 0
    instead. As a result, the core wasn't willing to send _any_ data for IN
    transactions.

--- src/chips/hotel/src/usb/constants.rs
+++ src/chips/hotel/src/usb/constants.rs
@@ -6,10 +6,12 @@ pub const USB_RESET: u32     = 1 << 12;
 pub const ENUM_DONE: u32     = 1 << 13;
 pub const IEPINT: u32     = 1 << 18;
 pub const OEPINT: u32     = 1 << 19;
+pub const GOUTNAKEFF: u32     = 1 << 7;
+pub const GINNAKEFF: u32     = 1 << 6;
 
 const MAX_CONTROL_ENDPOINTS: u16 = 3;
 const MAX_NORMAL_ENDPOINTS: u16  = 16;
-const MAX_PACKET_SIZE: u16        = 1024;
+const MAX_PACKET_SIZE: u16        = 64;
 //const FIFO_RAM_DEPTH: u16        = 1024;
 
 pub const RX_FIFO_SIZE: u16 = (4 * MAX_CONTROL_ENDPOINTS + 6) +
