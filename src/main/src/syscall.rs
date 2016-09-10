b31c244f4a3eb64062be8880fdf0b81d3ed397e1
Yuri Kunde Schlesner <yuriks@google.com>
2016-09-09 18:28:30 -0700

    Add SyscallError enum

--- src/main/src/syscall.rs
+++ src/main/src/syscall.rs
@@ -3,3 +3,27 @@ pub const SUBSCRIBE: u8 = 1;
 pub const COMMAND: u8 = 2;
 pub const ALLOW: u8 = 3;
 pub const MEMOP: u8 = 4;
+
+/// Possible errors returned by syscalls. In case of failure, the negative value of the error is
+/// returned to the application.
+#[derive(Copy, Clone)]
+pub enum SyscallError {
+    /// Generic errors that haven't been updated to use a more specific code yet.
+    Unknown = 1,
+    /// An argument passed is not (and never is) valid for this particular call.
+    InvalidArgument = 2,
+    /// An argument passed or operation attempted is not valid for the current state of the object.
+    InvalidState = 3,
+    /// A numeric argument is out-of-range, or a passed buffer is too small.
+    OutOfRange = 4,
+    /// The requested operation is unknown or unsupported.
+    NotImplemented = 5,
+    /// The resource is currently busy.
+    ResourceBusy = 6,
+}
+
+impl From<SyscallError> for isize {
+    fn from(e: SyscallError) -> Self {
+        -(e as isize)
+    }
+}
