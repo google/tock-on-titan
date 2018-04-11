8b8312bdda95a7b86fd8a58af872760701615757
Philip Levis <plevis@rodete-desktop-imager.corp.google.com>
2018-04-11 15:21:58 -0700

    Add RNG test.

---
+++ golf2/src/rng_test.rs
@@ -0,0 +1,16 @@
+use capsules::test::rng::TestRng;
+use kernel::hil::rng::{RNG, Continue, Client};
+use hotel::trng;
+
+pub unsafe fn run_rng() {
+    let r = static_init_test_rng();
+    trng::TRNG0.set_client(r);
+    r.run();
+}
+
+unsafe fn static_init_test_rng() -> &'static mut TestRng<'static> {
+    static_init!(
+        TestRng<'static>,
+        TestRng::new(&trng::TRNG0)
+    )
+}
