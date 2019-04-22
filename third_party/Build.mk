c93e506cc1b50b70db2c0c211da3a3fcb9d78daa
Johnathan Van Why <jrvanwhy@google.com>
2019-04-22 12:03:55 -0700

    Tell Cargo not to modify the Cargo.lock in elf2tab.
    Cargo likes to make extraneous changes to Cargo.lock (such as inserting comments) which makes elf2tab's workspace dirty (because elf2tab doesn't have Cargo.lock in .gitignore).

--- third_party/Build.mk
+++ third_party/Build.mk
@@ -26,7 +26,7 @@ third_party/build: build/cargo-host/release/elf2tab
 .PHONY: third_party/check
 third_party/check:
 	cd third_party/elf2tab && \
-		CARGO_BUILD_TARGET_DIR="../../build/cargo-host" cargo check
+		CARGO_BUILD_TARGET_DIR="../../build/cargo-host" cargo check --frozen
 
 .PHONY: third_party/devicetests
 third_party/devicetests:
@@ -34,16 +34,16 @@ third_party/devicetests:
 .PHONY: third_party/doc
 third_party/doc:
 	cd third_party/elf2tab && \
-		CARGO_BUILD_TARGET_DIR="../../build/cargo-host" cargo doc
+		CARGO_BUILD_TARGET_DIR="../../build/cargo-host" cargo doc --frozen
 
 .PHONY: third_party/localtests
 third_party/localtests:
 	cd third_party/elf2tab && \
-		CARGO_BUILD_TARGET_DIR="../../build/cargo-host" cargo test
+		CARGO_BUILD_TARGET_DIR="../../build/cargo-host" cargo test --frozen
 
 
 .PHONY: build/cargo-host/release/elf2tab
 build/cargo-host/release/elf2tab:
 	cd third_party/elf2tab && \
 		CARGO_BUILD_TARGET_DIR="../../build/cargo-host" \
-		cargo build --release
+		cargo build --frozen --release
