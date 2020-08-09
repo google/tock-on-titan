9bf3fec6b4025d571545222a1701bd14984ff814
Oskar Senft <osk@google.com>
2020-08-08 23:31:56 -0400

    Add "Enable/disable to wait for BUSY bit to clear before completing" to kernel.

--- kernel/h1/src/hil/spi_host.rs
+++ kernel/h1/src/hil/spi_host.rs
@@ -6,4 +6,16 @@ pub trait SpiHost {
     /// `enable`: Whether to enable (`true`) or disable (`false`) the pass
     /// through.
     fn spi_device_spi_host_passthrough(&self, enable: bool);
+
+    /// Enable/disable waiting for BUSY bit in status register of connected SPI
+    /// flash device to clear at the end of transactions by polling the status
+    /// register.
+    ///
+    /// If enabled, transactions are marked completed only when the BUSY bit
+    /// has been cleared. If disabled the the status register is not polled and
+    /// transaction are completed immediately.
+    ///
+    /// `enable`: Whether to enable (`true`) or disable (`false`) waiting for
+    /// the BUSY bit to be cleared.
+    fn wait_busy_clear_in_transactions(&self, enable: bool);
 }
